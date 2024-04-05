9d49998352d1eb5a19b1ed8e53cb0f3f8fc83154
Deepak Janardhanan <deepakramoham@gmail.com>
2024-04-05 14:11:44 +0530

    02 Create and render a browser router in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+/* This first route is what we often call the "root route" since the rest of our routes will render inside of it. 
+It will serve as the root layout of the UI, we'll have nested layouts as we get farther along. */
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div>Hi... Getting started with react Router</div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
