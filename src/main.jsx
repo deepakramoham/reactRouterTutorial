a0890563b7e15fa6ae925b9788dd078300a521d2
Deepak Janardhanan <deepakramoham@gmail.com>
2024-04-12 19:02:01 +0530

    JSX Routes _ createRoutesFromElements

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
 import "./index.css";
 import Root, {
   loader as rootLoader,
@@ -15,43 +21,40 @@ import EditContact, { action as editAction } from "./routes/Edit";
 import { action as destroyAction } from "./routes/Destroy";
 import Index from "./routes/Index";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          { index: true, element: <Index /> },
-          {
-            path: "contacts/:contactId",
-            element: <Contact />,
-            loader: contactLoader,
-            action: contactAction,
-          },
-          {
-            path: "contacts/:contactId/edit",
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction,
-          },
-          {
-            path: "contacts/:contactId/destroy",
-            errorElement: <div>Oops! There was an error.</div>,
-            action: destroyAction,
-          },
-        ],
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route path="contacts/:contactId/destroy" action={destroyAction} />
+      </Route>
+    </Route>
+  )
+);
 
-/* This first route is what we often call the "root route" since the rest of our routes will render inside of it. 
-It will serve as the root layout of the UI, we'll have nested layouts as we get farther along. */
+//JSX Routes
+/* And for our final trick, many folks prefer to configure their routes with JSX.
+ You can do that with createRoutesFromElements. 
+ There is no functional difference between JSX or objects when configuring your routes, it's simply a stylistic preference. */
+//That's it! Thanks for giving React Router a shot.
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
